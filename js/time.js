e12a9c1ca1c5cf454c85ce43a8a188ae85fcc8c6
NCCHANG <changhoehin2005@gmail.com>
2024-11-08 08:13:20 +0800

    changed onload update time

--- js/time.js
+++ js/time.js
@@ -1,12 +1,12 @@
 function updateTime() {
-
-const now = new Date();
-
-const hour = now.getHours();
-const minute = now.getMinutes();
-const time = hour + ":" + minute;
-
-document.getElementById("time").innerHTML = time;
+    
+    const now = new Date();
+    
+    const hour = now.getHours();
+    const minute = now.getMinutes();
+    const time = hour + ":" + minute;
+    
+    document.getElementById("time").innerHTML = time;
 }
-updateTime()
+document.body.onload = updateTime;
 setInterval(updateTime,1000)
