14751baddfb486b42af443ec7615bb6c991e885f
NCCHANG <changhoehin2005@gmail.com>
2024-11-10 18:04:50 +0800

    organised calender js abit

--- js/calender.js
+++ js/calender.js
@@ -1,3 +1,16 @@
+function getLastDateOfMonth(month,year) {
+    //get last day number of the month
+    let febLastDate;
+    if(year%4 === 0) {//is leap year
+        febLastDate = 29;
+    } else {
+        febLastDate = 28;
+    }
+    const lastDates = [31,febLastDate,31,30,31,30,31,31,30,31,30,31]; //index 0 is January
+    const lastDate = lastDates[month]; //get this month last date
+    return lastDate;
+}
+
 function daysBtns() {
     const tdy = new Date();
     //get date
@@ -9,16 +22,7 @@ function daysBtns() {
     const tdyMonth = monthNames[tdy.getMonth()]; //tdy.getMonth return num
                                                  //exp:February then ret 1
     
-    //get last day number of the month
-    let febLastDate;
-    if(tdy.getFullYear()%4 === 0) {//is leap year
-        febLastDate = 29;
-    } else {
-        febLastDate = 28;
-    }
-    const lastDates = [31,febLastDate,31,30,31,30,31,31,30,31,30,31];
-    const lastDate = lastDates[tdy.getMonth()]; //get this month last date
-    
+    const lastDate = getLastDateOfMonth(tdy.getMonth(),tdy.getFullYear());
 
     const setFirstOfMonth = new Date(`${tdyMonth} 01, 
         ${tdy.getFullYear()} 00:00:00`);
@@ -29,19 +33,20 @@ function daysBtns() {
         btn.appendChild(content);
         btn.id = `btn${i}`;
 
+        //STYLE
         const btnStyle = "border-width:1px;";
         btn.style = btnStyle;
         //set first button into correct day aka column
         //somehow borderwidth will get replaced by grid column
         //so need to type one more time
-        if(i === 1) {
+        if (i === 1) {
             btn.style = `grid-column: ${firstDayOfMonth};
             ${btnStyle}`
         }
-        //
         if (i == tdyDate) { //highlight current date
             btn.style = `background-color: #6d7897;${btnStyle}`;
         }
+        //
 
         const existing = document.querySelector("#calender");
         existing.appendChild(btn);
