1bcbf4a4601fe878ae931bebc387b00a378e7843
NCCHANG <changhoehin2005@gmail.com>
2024-11-10 18:51:11 +0800

    added second

--- js/time.js
+++ js/time.js
@@ -4,11 +4,15 @@ function updateTime() {
     
     const hour = now.getHours();
     const minute = now.getMinutes();
+    let second = now.getSeconds();
     let time;
+    if (second < 10) {
+        second = "0" + second;
+    }
     if(minute<10) {
-        time = hour + ":0" + minute;
+        time = hour + ":0" + minute +":"+ second;
     } else {
-        time = hour + ":" + minute;
+        time = hour + ":" + minute +":"+ second;
     }
     document.querySelector(".time").innerHTML = time;
 }
